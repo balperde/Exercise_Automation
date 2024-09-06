c5838f8b7f69298e9230526aefa61a389e9dc9d8
balperde <bekiralperdem@gmail.com>
2024-09-06 23:48:31 +0200

    Test updated

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -118,6 +118,8 @@ public class BaseTest {
 
 
 
+
+
     public void takeScreenShot(String fileName, WebElement element){
         TakesScreenshot screenshot = (TakesScreenshot) driver;
         File sourceFile, targetFile;
