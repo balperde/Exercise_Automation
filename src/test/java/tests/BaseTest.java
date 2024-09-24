70d112ba4ab27eeff5d944d8cd49b39ccba5ae10
balperde <bekiralperdem@gmail.com>
2024-09-24 20:54:05 +0200

    Test updated

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -137,9 +137,6 @@ public class BaseTest {
 
 
 
-
-
-
     public void selectHobby(String... hobbies) {
         String locator = "//div[./label[contains(., '%s')]]//input";
         for (String hobby : hobbies) {
